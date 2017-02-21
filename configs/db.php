322c857d40f1fde5e6e1508f1f72a080f6504277
wydewy <2631069976@qq.com>
2017-02-21 21:13:36 +0800

    1

--- configs/db.php
+++ configs/db.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'class' => '\vnique\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=vnique',
-    'username' => 'wydewy',
-    'password' => 'wydewy',
+    'dsn' => 'mysql:host=localhost;dbname=wydewy_db',
+    'username' => 'wydewy_user',
+    'password' => 'mariadb5.5_wydewy_user',
     'attributes' => [
         \PDO::ATTR_EMULATE_PREPARES => false,
         \PDO::ATTR_STRINGIFY_FETCHES => false,
---
+++ controllers/IndexController.php
@@ -0,0 +1,33 @@
+<?php
+namespace app\controllers;
+
+use vnique\web\Controller;
+use app\models\User;
+
+class IndexController extends Controller
+{
+    public function actionTest()
+    {
+		$user1 = new User();
+		$deviceId = "324241234";
+		$userName = "xxx";
+		$createDate = "20170920";
+		$telephone = "13632234322";
+		$enable = "0";
+		
+		$user1->insert();
+		
+        $data = [
+            'first' => 'awesome-php-zh_CN',
+            'second' => 'simple-framework',
+            'user' => $user1
+        ];
+        echo $this->toJson($data);
+    }
+    
+    public function actionView()
+    {
+        $this->render('site/view',['body' => 'hi vnique ~']);
+    }
+}
+
